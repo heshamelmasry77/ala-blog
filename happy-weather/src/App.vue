2a643978155e339a870c4287961c75f8f45a8573
Hesham <heshamelmasry1992@gmail.com>
2021-04-16 16:01:35 +0200

    project intializing

---
+++ happy-weather/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Dummy :header-name="'Hesham'" :main-paragraph="'hesham is a good man'"/>
+  <HelloWorld/>
+
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Dummy from './components/Dummy.vue'
+export default {
+  name: 'App',
+  components: {
+    Dummy,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
